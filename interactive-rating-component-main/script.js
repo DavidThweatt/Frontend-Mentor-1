076549fd1875d4c23cd061db4e3abb70ada29755
David Thweatt <68977760+DavidThweatt@users.noreply.github.com>
2022-03-22 12:57:40 -0700

    Rating page styling
    Rating page styling done

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,15 @@
+const submitBTN = document.getElementsByClassName("RatingBtn");
+const numRating = document.getElementsByClassName("Rating");
+
+// numRating.addEventListener("click", (e) => {
+//   activeRating(e);
+// });
+
+submitBTN.addEventListener("click", (event) => {
+  console.log(hello);
+});
+
+// function activeRating(e) {
+//   e.target.addClass("active").siblings().removeClass("active");
+//   console.log(hello);
+// }

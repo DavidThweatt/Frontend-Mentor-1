524115af30968d4caa0a8ed6050bde0349a59067
David Thweatt <68977760+DavidThweatt@users.noreply.github.com>
2022-03-22 16:09:18 -0700

    css and javascript
    submitted page styled.
    most logic in place

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,15 +1,28 @@
-const submitBTN = document.getElementsByClassName("RatingBtn");
-const numRating = document.getElementsByClassName("Rating");
+const submit = document.getElementsByClassName("RatingBtn");
+const rating = document.getElementsByClassName("Rating");
+const rateDiv = document.getElementsByClassName("rateContainer");
+const submitDiv = document.getElementsByClassName("submitContainer");
+const num = document.getElementsByClassName("ratePick");
 
-// numRating.addEventListener("click", (e) => {
-//   activeRating(e);
-// });
+for (let i = 0; i < rating.length; i++) {
+  rating[i].addEventListener("click", (e) => {
+    const current = document.getElementsByClassName("activeRate");
 
-submitBTN.addEventListener("click", (event) => {
-  console.log(hello);
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" activeRate", "");
+    }
+
+    // Add the active class to the current/clicked button
+    e.target.className += " activeRate";
+
+    console.log([i]);
+    const newNum = document.createTextNode("[i + 1]");
+    num.appendChild(newNum);
+  });
+}
+
+submit[0].addEventListener("click", () => {
+  rateDiv[0].style.display = "flex" ? "none" : "flex";
+  submitDiv[0].style.display = "none" ? "flex" : "none";
 });
-
-// function activeRating(e) {
-//   e.target.addClass("active").siblings().removeClass("active");
-//   console.log(hello);
-// }
